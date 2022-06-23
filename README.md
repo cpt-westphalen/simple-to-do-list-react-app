b6d41456270ba575816bc096b7265d9ec57a9c90
cpt-westphalen <rbwestphalen@gmail.com>
2022-06-23 20:17:55 -0300

    app should be working fine

---
+++ README.md
@@ -0,0 +1,7 @@
+# simple-todo-react-app
+
+Iniciei estudos em React em junho/2022, esse foi um pequeno produto desse estudo inicial.
+
+## English
+
+This simple single page app was made as an initial exercise for learning React.
---
+++ src/UserInput.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function UserInput({ className, addTask }) {
+  const [inputTask, setInputTask] = useState("");
+
+  const handleClick = () => {
+    //check if not empty
+    if (inputTask.trim()) addTask(inputTask);
+    //clean up the input
+    document.getElementById("input-field").value = "";
+    setInputTask("");
+  };
+
+  const handleInputChange = (e) => {
+    setInputTask(e.target.value);
+  };
+
+  const handleKey = (k) => {
+    if (k.key === "Enter") {
+      k.preventDefault();
+      handleClick();
+    }
+  };
+
+  return (
+    <div className={className}>
+      <input
+        id="input-field"
+        onKeyDown={handleKey}
+        onChange={handleInputChange}
+        type="text"
+      />
+      <button onClick={handleClick}>Add task</button>
+    </div>
+  );
+}
